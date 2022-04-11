b870333a50dadbe1b508bbaa7bad6f1c11bb67ee
KYLE DEAN <b9015015@hallam.shu.ac.uk>
2022-04-11 07:10:38 +0100

    first commit

---
+++ components/index.js
@@ -0,0 +1,21 @@
+import HomeHeader from './HomeHeader'
+import FocusedStatusBar from './FocusedStatusBar'
+import NFTCard from './NFTCard'
+import { CircleButton, RectButton } from './Button'
+import { NFTTitle, EthPrice, ImageCmp, People, EndDate, SubInfo } from './SubInfo'
+
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  
+  CircleButton,
+  RectButton,
+
+  NFTTitle,
+  EthPrice,
+  ImageCmp,
+  People,
+  EndDate,
+  SubInfo
+}
