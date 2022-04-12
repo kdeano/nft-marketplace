e2026dcb72f303a69d18c381845975e6265693ee
KYLE DEAN <b9015015@hallam.shu.ac.uk>
2022-04-12 07:23:13 +0100

    Finished the development of the UI/UX

--- screens/Details.js
+++ screens/Details.js
@@ -63,6 +63,16 @@ const Details = ({ route, navigation }) => {
             <SubInfo />
             <View style={{ padding: SIZES.font }}>
               <DetailsDesc data={data} />
+
+              {data.bids.length > 0 && (
+                <Text style={{
+                  fontSize: SIZES.font,
+                  fontFamily: FONTS.semiBold,
+                  color: COLORS.primary
+                }}>
+                  Current bids
+                </Text>
+              )}
             </View>
           </React.Fragment>
         )}
